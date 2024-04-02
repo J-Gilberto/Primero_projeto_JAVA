811f0655e009bb16e865aae96c53003bb8c5ceee
J.Gilberto <mr.joaogilberto@hotmail.com>
2024-04-02 11:14:14 -0300

    Aprendendo Java (Forma dificil)

---
+++ Teste.java
@@ -0,0 +1,5 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Gear of Wars " + 8 * 3);
+    }
+}
---
+++ src/Word.java
@@ -0,0 +1,6 @@
+public class Word {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Fim de Mundo!");
+    }
+}
